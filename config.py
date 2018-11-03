4c05fb0a7763e611143318557097953f35be2cf4
flask04 <flask04@163.com>
2018-11-03 15:57:17 +0800

    配置代码抽取——Config

---
+++ config.py
@@ -0,0 +1,21 @@
+import redis
+
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SECRET_KEY = "UAXpxgekKf7reqOqHwKPTnJfBwJtA0bd6HkvOiLmGiMm7IAGskyaDpHrSS23OR/P"
+
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask04_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定session保存到redis中
+    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session的有效期，单位是秒
--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import redis
+from config import Config
 from flask import Flask
 from flask_session import Session
 from flask.ext.wtf import CSRFProtect
@@ -7,27 +8,6 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-    SECRET_KEY = "UAXpxgekKf7reqOqHwKPTnJfBwJtA0bd6HkvOiLmGiMm7IAGskyaDpHrSS23OR/P"
-
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask04_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定session保存到redis中
-    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session的有效期，单位是秒
-
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
