0df9613b40344609d12cabc9e3413974ba1563fb
flask04 <flask04@163.com>
2018-11-03 15:30:56 +0800

    Session——利用flask-session扩展，将session数据保存到Redis中

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import redis
 from flask import Flask
+from flask_session import Session
 from flask.ext.wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,17 +10,28 @@ app = Flask(__name__)
 class Config(object):
     """工程配置信息"""
     DEBUG = True
+    SECRET_KEY = "UAXpxgekKf7reqOqHwKPTnJfBwJtA0bd6HkvOiLmGiMm7IAGskyaDpHrSS23OR/P"
+
     # 数据库的配置信息
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask04_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # redis配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定session保存到redis中
+    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session的有效期，单位是秒
+
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
+Session(app)
 
 
 @app.route("/")
