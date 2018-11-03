5e8fdea50dbc866046e3ff577f3eff6463cefd03
flask04 <flask04@163.com>
2018-11-03 09:31:06 +0800

    Config类----SQLAlchemy

--- manage.py
+++ manage.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask04_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route("/")
