c34b0a3190990d1c470bf91d649c1c29c7e17535
flask04 <flask04@163.com>
2018-11-03 15:52:33 +0800

    flask-script与数据库迁移

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@ import redis
 from flask import Flask
 from flask_session import Session
 from flask.ext.wtf import CSRFProtect
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,6 +34,9 @@ db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 Session(app)
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 
 @app.route("/")
@@ -40,4 +45,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    manager.run()
